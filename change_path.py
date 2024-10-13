d05e54badaab25aea3184220073bb5b7d395913a
junhojo <charm1109@naver.com>
2024-10-13 20:57:26 +0900

    Update change_path.py

--- change_path.py
+++ change_path.py
@@ -2,14 +2,15 @@ import os
 import re
 # 아이폰 폴더링 
 # 변경할 폴더 경로
-folder_path = '/Users/jojunho/Desktop/0815_backup'
+folder_path = '/Users/jojunho/Desktop/1013_backup'
 
 # 폴더 내의 모든 파일 및 폴더 이름 가져오기
 print(f"폴더 경로: {folder_path}")
 print("파일 및 폴더 이름을 분석하고 변경하는 중...\n")
 
 # 정규 표현식 패턴 (날짜와 관련된 부분을 찾는 패턴)
-date_pattern = re.compile(r"(.*?), (\d{4})년 (\d{1,2})월 (\d{1,2})일")
+# date_pattern = re.compile(r"(.*?), (\d{4})년 (\d{1,2})월 (\d{1,2})일")
+date_pattern = re.compile(r"(.*?),?\s*(\d{4})\s*년\s*(\d{1,2})\s*월\s*(\d{1,2})\s*일")
 
 # 폴더 경로 내의 파일 및 폴더 이름 순회
 for folder_name in os.listdir(folder_path):
@@ -28,7 +29,7 @@ for folder_name in os.listdir(folder_path):
             d = match.group(4).zfill(2)  # 일 (두 자리 숫자로 변환)
             
             # 새로운 폴더 이름 생성 (m + d + "_" + x)
-            new_folder_name = f"{m}{d}_{x}"
+            new_folder_name = f"{m}{d}_{x}" if x else f"{m}{d}_"
             
             # 년도별 하위 폴더 경로 생성 (폴더가 없으면 생성)
             year_folder_path = os.path.join(folder_path, y)
